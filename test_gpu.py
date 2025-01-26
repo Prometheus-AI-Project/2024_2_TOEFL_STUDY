d0480366ac246795ca0675c46c39d90c15b9de7c
firepool7030 <firepool77@gmail.com>
2025-01-26 23:52:02 +0900

    Removed all files from gh-pages branch

--- test_gpu.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print("torch.cuda.is_available():", torch.cuda.is_available())
-print("torch.cuda.device_count():", torch.cuda.device_count())
-print("torch.cuda.current_device():", torch.cuda.current_device())
-print("torch.cuda.get_device_name():", torch.cuda.get_device_name(0))
